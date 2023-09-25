13616febc51e74b791686e4583ea5b1e71c34a63
Niamh Mallott <niamhmallott@hotmail.co.uk>
2023-09-25 09:36:37 +0100

    Render gusses component in game component and set state.

--- src/components/Game/Guesses.js
+++ src/components/Game/Guesses.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
 
 export function Guesses() {
+  const [userInput, setUserInput] = useState("");
+
   return (
     <form class="guess-input-wrapper">
       <label for="guess-input">Enter guess:</label>
-      <input id="guess-input" type="text" />
+      <input
+        id="guess-input"
+        type="text"
+        onChange={(event) => {
+          setUserInput(event.target.value);
+        }}
+      />
     </form>
   );
 }
