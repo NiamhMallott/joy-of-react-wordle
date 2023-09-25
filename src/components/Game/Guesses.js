b662c1b442f2a78cf97407342edd5f5b4597501f
Niamh Mallott <niamhmallott@hotmail.co.uk>
2023-09-25 10:31:51 +0100

    set the min and max letters

--- src/components/Game/Guesses.js
+++ src/components/Game/Guesses.js
@@ -4,13 +4,24 @@ export function Guesses() {
   const [userInput, setUserInput] = useState("");
 
   return (
-    <form class="guess-input-wrapper">
-      <label for="guess-input">Enter guess:</label>
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log(userInput);
+        setUserInput("");
+      }}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
       <input
         id="guess-input"
         type="text"
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        value={userInput}
         onChange={(event) => {
-          setUserInput(event.target.value);
+          setUserInput(event.target.value.toUpperCase());
         }}
       />
     </form>
--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,2 +1,3 @@
 export * from "./Game";
+export * from "./Guesses";
 export { default } from "./Game";
